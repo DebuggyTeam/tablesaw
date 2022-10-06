2ad62f413cb2a644a6346375630e79db981402c8
Falkreon <falkreon@gmail.com>
2022-10-06 08:14:29 -0500

    VoxelShape and translation key for tablesaw

--- src/main/java/io/github/debuggyteam/tablesaw/TableSawBlock.java
+++ src/main/java/io/github/debuggyteam/tablesaw/TableSawBlock.java
@@ -1,6 +1,5 @@
 package io.github.debuggyteam.tablesaw;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.block.StonecutterBlock;
@@ -8,29 +7,46 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.ScreenHandlerContext;
 import net.minecraft.screen.SimpleNamedScreenHandlerFactory;
-import net.minecraft.screen.StonecutterScreenHandler;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
 
 public class TableSawBlock extends StonecutterBlock {
-    private static final Text GUI_TITLE = Text.translatable("container.tablesaw");
-    protected static final VoxelShape CUBOID = Block.createCuboidShape(0, 0, 0, 16 , 16, 16);
-
+	// Think of this as "minecraft:container/tablesaw:tablesaw"
+	// the concept of a "container" is owned by vanilla, but we own this particular container and need to namespace it
+	// for protection in case we need to put a second container block in.
+    private static final Text GUI_TITLE = Text.translatable("container.tablesaw.tablesaw");
+    
+    private static final double PX = 1/16d;
+    private static final double TOP_THICKNESS = 3*PX;
+    private static final double FULL_HEIGHT = 16*PX;
+    private static final double FULL_WIDTH = 16*PX;
+    private static final double LEG_WIDTH = 3*PX;
+    private static final double LEG_HEIGHT = FULL_HEIGHT - TOP_THICKNESS;
+    
+    protected static final VoxelShape TOP_PLATE = VoxelShapes.cuboid(0, FULL_HEIGHT - TOP_THICKNESS, 0, FULL_WIDTH, FULL_HEIGHT, FULL_WIDTH);
+    protected static final VoxelShape NW_LEG = VoxelShapes.cuboid(0,0,0,LEG_WIDTH,LEG_HEIGHT,LEG_WIDTH);
+    protected static final VoxelShape NE_LEG = VoxelShapes.cuboid(FULL_WIDTH-LEG_WIDTH, 0, 0, FULL_WIDTH, LEG_HEIGHT, LEG_WIDTH);
+    protected static final VoxelShape SW_LEG = VoxelShapes.cuboid(0,0,FULL_WIDTH-LEG_WIDTH, LEG_WIDTH, LEG_HEIGHT, FULL_WIDTH);
+    protected static final VoxelShape SE_LEG = VoxelShapes.cuboid(FULL_WIDTH-LEG_WIDTH, 0, FULL_WIDTH-LEG_WIDTH, FULL_WIDTH, LEG_HEIGHT, FULL_WIDTH);
+    
+    protected static final VoxelShape SHAPE = VoxelShapes.union(TOP_PLATE, NW_LEG, NE_LEG, SW_LEG, SE_LEG);
+    
     public TableSawBlock(Settings settings) {
         super(settings);
     }
 
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
-        return CUBOID;
+        return SHAPE;
     }
 
     @Override

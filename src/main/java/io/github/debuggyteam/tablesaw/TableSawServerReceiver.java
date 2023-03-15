110dd6644e75ebbb6bcc0d204db4d5e22c853ac1
woodiertexas <woodytexas8088@gmail.com>
2023-03-15 14:41:43 -0700

    lets hope the trailing indents don't get yeeted this time

--- src/main/java/io/github/debuggyteam/tablesaw/TableSawServerReceiver.java
+++ src/main/java/io/github/debuggyteam/tablesaw/TableSawServerReceiver.java
@@ -1,13 +1,12 @@
 package io.github.debuggyteam.tablesaw;
 
-import org.quiltmc.qsl.networking.api.PacketSender;
-import org.quiltmc.qsl.networking.api.ServerPlayNetworking;
-
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
+import org.quiltmc.qsl.networking.api.PacketSender;
+import org.quiltmc.qsl.networking.api.ServerPlayNetworking;
 
 public class TableSawServerReceiver implements ServerPlayNetworking.ChannelReceiver {
 

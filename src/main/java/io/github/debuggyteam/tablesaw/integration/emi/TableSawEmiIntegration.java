57cc52d6a07ad060250cc6d1be70977d08225d4e
Falkreon <falkreon@gmail.com>
2023-07-01 22:48:20 -0500

    bump to 1.20.1, re-add emi integration, fix cullface bug

--- src/main/java/io/github/debuggyteam/tablesaw/integration/emi/TableSawEmiIntegration.java
+++ src/main/java/io/github/debuggyteam/tablesaw/integration/emi/TableSawEmiIntegration.java
@@ -1,17 +1,17 @@
 package io.github.debuggyteam.tablesaw.integration.emi;
-/*
+
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
 import dev.emi.emi.api.recipe.EmiRecipeCategory;
 import dev.emi.emi.api.stack.EmiStack;
-*/
+
 import io.github.debuggyteam.tablesaw.TableSaw;
 import io.github.debuggyteam.tablesaw.TableSawRecipes;
 import io.github.debuggyteam.tablesaw.api.TableSawRecipe;
 import net.minecraft.util.Identifier;
 
-public class TableSawEmiIntegration /*implements EmiPlugin*/ {
-	/*
+public class TableSawEmiIntegration implements EmiPlugin {
+	
 	public static final EmiStack TABLESAW_BLOCK = EmiStack.of(TableSaw.TABLESAW);
 	public static final EmiRecipeCategory TABLESAW_CATEGORY = new EmiRecipeCategory(new Identifier(TableSaw.MODID, "tablesaw"), EmiStack.of(TableSaw.TABLESAW));
 	
@@ -23,5 +23,5 @@ public class TableSawEmiIntegration /*implements EmiPlugin*/ {
 		for (TableSawRecipe recipe : TableSawRecipes.serverInstance().queueAll()) {
 			registry.addRecipe(new TableSawEmiRecipe(recipe));
 		}
-	}*/
+	}
 }
